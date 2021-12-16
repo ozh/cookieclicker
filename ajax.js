7fd955ae627503a2a7c4e7ed1ef51ef713a83841
removedporn <86824510+removedporn@users.noreply.github.com>
2021-12-16 18:34:32 +0800

    Delete ajax.js

--- ajax.js
+++
@@ -1,6 +0,0 @@
-function ajax(url,callback){
-	var ajaxRequest;
-	try{ajaxRequest = new XMLHttpRequest();} catch (e){try{ajaxRequest=new ActiveXObject('Msxml2.XMLHTTP');} catch (e) {try{ajaxRequest=new ActiveXObject('Microsoft.XMLHTTP');} catch (e){alert("Something broke!");return false;}}}
-	if (callback){ajaxRequest.onreadystatechange=function(){if(ajaxRequest.readyState==4){callback(ajaxRequest.responseText);}}}
-	ajaxRequest.open('GET',url+'&nocache='+(new Date().getTime()),true);ajaxRequest.send(null);
-}
