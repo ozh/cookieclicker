9373f4c7ff48f53cbedfcaa622e783fc8538ea3c
bryrynnyyr <143289585+bryrynnyyr@users.noreply.github.com>
2023-08-26 22:19:50 -0500

    Update main.js

--- main.js
+++ main.js
@@ -16861,10 +16861,7 @@ window.onload=function()
 				locStringsFallback=locStrings;
 				LoadLang('loc/'+lang+'.js?v='+Game.version,function(){
 					var launch=function(){
-						Game.Launch();
-						if (top!=self) Game.ErrorFrame();
-						else
-						{
+						
 							console.log('[=== '+choose([
 								'Oh, hello!',
 								'hey, how\'s it hangin',
@@ -16875,7 +16872,6 @@ window.onload=function()
 							Game.Load(function(){Game.Init();if (firstLaunch) Game.showLangSelection(true);});
 							//try {Game.Load(Game.Init);}
 							//catch(err) {console.log('ERROR : '+err.message);}
-						}
 					}
 					if (App && App.loadMods) App.loadMods(launch);
 					else launch();
